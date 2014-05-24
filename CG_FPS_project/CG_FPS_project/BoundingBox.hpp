518a9dfeb197962b53bf51d61d15a00f5232e10d
aclima93 <tocalagarto93@gmail.com>
2014-05-24 19:59:25 +0100

    - rudimentos da bullet collision detetction com raytracing (ainda não funcional)

--- CG_FPS_project/CG_FPS_project/BoundingBox.hpp
+++ CG_FPS_project/CG_FPS_project/BoundingBox.hpp
@@ -14,11 +14,13 @@ class BoundingBox{
         float x, y, z;
         float w, l, h;
         float halfW, halfH, halfL;
+        int id;
 
 
         BoundingBox(){}
 
-        void Init(float xx, float yy, float zz, float ww, float ll, float hh){
+        void Init(int idd, float xx, float yy, float zz, float ww, float ll, float hh){
+            id = id;
             x = xx;
             y = yy;
             z = zz;
@@ -80,6 +82,46 @@ class BoundingBox{
 
 
             glPopMatrix();
+        }
+
+        float checkCollision(float ox, float oy, float oz,  //origin of ray
+                             float dx, float dy, float dz){ // direction of ray
+
+            // AABB = Axis Aligned Bounding Box
+
+            float xFrac = 1.0f / dx;
+            float yFrac = 1.0f / dy;
+            float zFrac = 1.0f / dz;
+
+            // lb is the corner of AABB with minimal coordinates, rt is maximal corner
+
+            /// TODO: ver em tempo real qual é o ponto mais afastado e o mais próximo
+            /// (são só quatro pontos e calcular a distância a cada um deles)
+
+            float t1 = (lb.x - ox)*xFrac;
+            float t2 = (rt.x - ox)*xFrac;
+            float t3 = (lb.y - oy)*yFrac;
+            float t4 = (rt.y - oy)*yFrac;
+            float t5 = (lb.z - oz)*zFrac;
+            float t6 = (rt.z - oz)*zFrac;
+
+            float tmin = max( max( min(t1, t2), min( t3, t4)), min( t5, t6) );
+            float tmax = min( min( max(t1, t2), max( t3, t4)), max( t5, t6) );
+
+            // if tmax < 0, ray (line) is intersecting AABB, but whole AABB is behing us
+            if (tmax < 0)
+            {
+                return -1;
+            }
+
+            // if tmin > tmax, ray doesn't intersect AABB
+            if (tmin > tmax)
+            {
+                return -1;
+            }
+
+            return tmin;
+
         }
 
 
